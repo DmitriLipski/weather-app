bd4692933b89ae37db3db7a5c55eb49707d957c3
dmitri_lipski <dzmitri.lipski@gmail.com>
2019-03-31 14:46:03 +0300

    Done code refactoring

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('request');
 const forecast = (latitude, longitude, callback) => {
     const url = `https://api.darksky.net/forecast/90bca0723f568b56588333faa7d586c2/${latitude},${longitude}?units=si`;
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', null)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', null)
         } else {
-            callback(null, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+            callback(null, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 };

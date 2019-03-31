9ddc31a8003fc4397e32b4c5789a8d38eb4f0ef5
dmitri_lipski <dzmitri.lipski@gmail.com>
2019-03-31 14:31:41 +0300

    Chane requests

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode("Minsk", (error, data) =>{
-   console.log("Error", error);
-   console.log("Data", data);
-});
+const address = process.argv[2];
 
-forecast(27.56184, 53.90225, (error, data) => {
-    if (error) {
-        console.log("Error", error);
-        return
-    }
-    console.log(data.summary + ' It is currently ' + data.temperature + ' degress out. There is a ' + data.precipProbability + '% chance of rain.')
-});
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+
+            console.log(data.location);
+            console.log(forecastData)
+        })
+    })
+}
 
 
--- utils/forecast.js
+++ utils/forecast.js
@@ -9,11 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (response.body.error) {
             callback('Unable to find location', null)
         } else {
-            callback(null, {
-                summary: response.body.daily.data[0].summary,
-                temperature: response.body.currently.temperature,
-                precipProbability: response.body.currently.precipProbability
-            })
+            callback(null, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
         }
     })
 };
